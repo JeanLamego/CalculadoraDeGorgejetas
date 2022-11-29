4f1f6e9f33e9b0f92226864b7de58a4ff9aae161
k0edb <jeanlamego@hotmail.com>
2022-11-29 16:54:30 -0300

    começando o js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// function valorDaConta(){
+//     document.getElementById("bill").value;
+//     console.log(bill);
+// }
+
+function calcularGorjeta(){
+    var gorjeta = document.getElementById("bill").value;
+    alert("valor da conta" + gorjeta);
+}
