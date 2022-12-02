b078c963e7273bd1c69a739f6b16f45d45c19595
k0edb <jeanlamego@hotmail.com>
2022-12-01 22:59:36 -0300

    Falta botão custom

--- js/main.js
+++ js/main.js
@@ -1,52 +1,57 @@
   
-// function valorDaConta(){
-//     const gorjeta = document.getElementById("bill").value;
-//     if (gorjeta>0){
-//         alert(gorjeta);
-//     }else{
-//         alert("Você precisar por um valor maior que Zero")
-//     }
-//     console.log(gorjeta);
-//     // return gorjeta;
-// };
-
-// function numberOfPeople(){
-//     const quantidade = document.getElementById("number-of-people").value;
-//     if (quantidade>0){
-//         alert(quantidade);
-//     }else{
-//         alert("A quantidade de pessoas não pode ser menor que 1");
-//     }
-// };
-
-    const gorjeta = document.getElementById("bill");
-    const quantidade = document.getElementById("number-of-people");
-    
-    function valorDaConta(){    
-        var inputGorjeta= gorjeta.value;
-        if (inputGorjeta >0){
-            return inputGorjeta;
-        }else
-        return alert("O valor da conta não pode ser zero");
+  // function valorDaConta(){
+  //     const gorjeta = document.getElementById("bill").value;
+  //     if (gorjeta>0){
+  //         alert(gorjeta);
+  //     }else{
+  //         alert("Você precisar por um valor maior que Zero")
+  //     }
+  //     console.log(gorjeta);
+  //     // return gorjeta;
+  // };
+  
+  // function numberOfPeople(){
+  //     const quantidade = document.getElementById("number-of-people").value;
+  //     if (quantidade>0){
+  //         alert(quantidade);
+  //     }else{
+  //         alert("A quantidade de pessoas não pode ser menor que 1");
+  //     }
+  // };
+  
+  const gorjeta = document.getElementById("bill");
+  const quantidade = document.getElementById("number-of-people");
+  
+  
+  function valorDaConta(){    
+    var inputGorjeta= gorjeta.value;
+    if (inputGorjeta >0){
+        return inputGorjeta;
+    }else
+    return alert("O valor da conta não pode ser zero");
 };
-    function numberOfPeople(){
-        var inputQuantidade = quantidade.value;
-
-        if(inputQuantidade>0){
-            return inputQuantidade;
-        }else
-        return alert("O número de pessoas não pode ser zero");
+function numberOfPeople(){
+    var inputQuantidade = quantidade.value;
+    
+    if(inputQuantidade>0){
+        return inputQuantidade;
+    }else
+    return alert("O número de pessoas não pode ser zero");
 };
 
 function calcularGorjeta(porcentagem){
-    console.log("Valor da Conta " + valorDaConta());
-    console.log("Quantidade de Pessoas " + numberOfPeople());
-
+    // console.log("Valor da Conta " + valorDaConta());
+    // console.log("Quantidade de Pessoas " + numberOfPeople());
     let valorGorjeta = valorDaConta() * (Number(porcentagem)/100) / numberOfPeople();
-    
-    console.log("Valor da Gorjeta " + valorGorjeta);
-    return valorGorjeta;
+    let totalPorPessoa = valorDaConta()/ numberOfPeople();
+    var tipsPerPerson = document.querySelector(".tip-amount-result");
+    tipsPerPerson.innerHTML="R$" + valorGorjeta.toFixed(2);
+    // console.log("Valor da Gorjeta " + valorGorjeta);
 
+    var totalPerPerson = document.querySelector(".total-result");
+    totalPerPerson.innerHTML="R$" + totalPorPessoa.toFixed(2);
+    return valorGorjeta;
+    
 };
 
 
