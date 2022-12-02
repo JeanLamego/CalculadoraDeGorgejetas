10309a2029f16196b5c093a21c40f6791aa02542
k0edb <jeanlamego@hotmail.com>
2022-12-01 22:34:22 -0300

    js surgindo

--- js/main.js
+++ js/main.js
@@ -1,37 +1,52 @@
+  
 // function valorDaConta(){
-//     document.getElementById("bill").value;
-//     console.log(bill);
-// }
+//     const gorjeta = document.getElementById("bill").value;
+//     if (gorjeta>0){
+//         alert(gorjeta);
+//     }else{
+//         alert("Você precisar por um valor maior que Zero")
+//     }
+//     console.log(gorjeta);
+//     // return gorjeta;
+// };
 
+// function numberOfPeople(){
+//     const quantidade = document.getElementById("number-of-people").value;
+//     if (quantidade>0){
+//         alert(quantidade);
+//     }else{
+//         alert("A quantidade de pessoas não pode ser menor que 1");
+//     }
+// };
 
-
-
-
-
-
-
-const valorDaConta = function (){
-    var gorjeta = document.getElementById("bill").value;
-    if (gorjeta>0){
-        alert(gorjeta);
-    }else{
-        alert("Você precisar por um valor maior que Zero")
-    }
-    // return gorjeta;
-   
-};
-
-numberOfPeople = function (){
-    var quantidade = document.getElementById("number-of-people").value;
-    if (quantidade>0){
-        alert(quantidade);
-    }else{
-        alert("A quantidade de pessoas não pode ser menor que 1");
-    }
-};
-
-    function calcularGorjeta(){
-    var calcular= numberOfPeople(quantidade)*valorDaConta(gorjeta);
+    const gorjeta = document.getElementById("bill");
+    const quantidade = document.getElementById("number-of-people");
     
- console.log(calcular);
-    }
+    function valorDaConta(){    
+        var inputGorjeta= gorjeta.value;
+        if (inputGorjeta >0){
+            return inputGorjeta;
+        }else
+        return alert("O valor da conta não pode ser zero");
+};
+    function numberOfPeople(){
+        var inputQuantidade = quantidade.value;
+
+        if(inputQuantidade>0){
+            return inputQuantidade;
+        }else
+        return alert("O número de pessoas não pode ser zero");
+};
+
+function calcularGorjeta(porcentagem){
+    console.log("Valor da Conta " + valorDaConta());
+    console.log("Quantidade de Pessoas " + numberOfPeople());
+
+    let valorGorjeta = valorDaConta() * (Number(porcentagem)/100) / numberOfPeople();
+    
+    console.log("Valor da Gorjeta " + valorGorjeta);
+    return valorGorjeta;
+
+};
+
+
